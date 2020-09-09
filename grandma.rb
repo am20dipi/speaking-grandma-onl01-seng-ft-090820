cb8c53336635dd4032afdd6b55d8722437af6754
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-09 16:00:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,9 @@ def speak_to_grandma(phrase)
      'I LOVE YOU TOO PUMPKIN!'
   elsif phrase != phrase.upcase 
       'HUH?! SPEAK UP, SONNY!'
-  elsif phrase == phrase.upcase && phrase != 'I LOVE YOU GRANDMA!'   'NO, NOT SINCE 1938!'
+  elsif phrase == phrase.upcase 
+    if phrase != 'I LOVE YOU GRANDMA!'   
+      'NO, NOT SINCE 1938!'
   end
 end
  
